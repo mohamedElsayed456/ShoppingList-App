94b9c000d48e7fb39a512af463e077b7c5bbc2d4
mohamedElsayed456 <mohamedelsayed0500@gmail.com>
2023-09-17 19:29:30 +0300

    add futureBuilder

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,34 +16,25 @@ class GroceryList extends StatefulWidget{
 class _GroceryListState extends State<GroceryList>{
 
 List<GroceryItem>groceryItems=[];
-
-var isloading = true;
-String? _error;
+late Future<List<GroceryItem>>_loadedItems;
 
  @override
   void initState(){
     super.initState();
-    loadItems();
+    _loadedItems =_loadItems();
   }
 
-void loadItems()async{
-  final url = Uri.https('flutter-prep-664a7-default-rtdb.firebaseio','shopping_list.json');
-
-  try{
+Future<List<GroceryItem>> _loadItems()async{
+  final url = Uri.https('flutter-prep-664a7-default-rtdb.firebaseio.com','shopping_list.json');
 
     final response = await http.get(url);
   
     if(response.statusCode>=400){
-    setState(() {
-       _error='Failed to fetch data,Please try again later';
-    });
-   }
+          throw Exception('Failed to fetch grocery items. Please try again later.');   
+     }
 
    if(response.body=='null'){
-    setState(() {
-      isloading=false;
-    });
-    return;
+    return [];
    }
 
   final Map<String,dynamic> listData= json.decode(response.body);
@@ -59,20 +50,11 @@ void loadItems()async{
         quantity:item.value['quantity'],
         category:category,
         )
-      );
+      ); 
     }
-    setState(() {
-       groceryItems = loadedItems;
-       isloading=false;
-    });
-  }catch(error){
-   setState((){
-       _error ='something went wrong! Please try again later';
-    });
-  }
-  
-   
+    return loadedItems;
 }
+
 void addItem()async{
  final newItem = await Navigator.of(context).push<GroceryItem>(
   MaterialPageRoute(builder: (ctx)=>const NewItem()
@@ -100,44 +82,10 @@ void addItem()async{
        groceryItems.insert(index,item);
         });
       }
-    
    }
 
   @override
   Widget build(BuildContext context){
-
-     Widget content =const Center(child: Text('No items added yet.'),);
-
-     if(isloading){
-      content = const Center(child: CircularProgressIndicator(),);
-     }
-
-     if(groceryItems.isNotEmpty){
-      content = ListView.builder(
-        itemCount:groceryItems.length,
-        itemBuilder:(ctx,index) => Dismissible(
-          onDismissed:(direction){
-            removeItem(groceryItems[index]);
-          },
-          key:ValueKey (groceryItems[index].id), 
-          child: ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-           ), 
-           trailing:Text(groceryItems[index].quantity.toString()
-           ),
-          ),
-         ),
-        );
-      }
-
-     if(_error!=null){
-      content = Center(child: Text(_error!));
-     }
-
     return Scaffold(
       appBar: AppBar(
         title:const Text('Your Groceries'),
@@ -145,11 +93,44 @@ void addItem()async{
           IconButton(
             onPressed:addItem,
             icon:const Icon(Icons.add),
-             )
-        ],
+             ), 
+          ],
       ),
       
-      body:content,
+      body:FutureBuilder(
+        future:_loadedItems,
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+              return const Center(child: CircularProgressIndicator(),
+            );
+          }
+          if(snapshot.hasError){
+              return Center(child: Text(snapshot.error.toString()));
+            }
+          if(snapshot.data!.isEmpty){
+            return const Center(child: Text('No items added yet.'),);
+          }
+          return ListView.builder(
+          itemCount:snapshot.data!.length,
+          itemBuilder:(ctx,index) => Dismissible(
+          onDismissed:(direction){
+            removeItem(snapshot.data![index]);
+          },
+          key:ValueKey (snapshot.data![index].id), 
+          child: ListTile(
+          title: Text(snapshot.data![index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: snapshot.data![index].category.color,
+           ), 
+           trailing:Text(snapshot.data![index].quantity.toString()
+           ),
+          ),
+         ),
+        );
+       },
+      ),
     );
   }
 }
